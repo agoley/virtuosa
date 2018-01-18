294cf6e71d1881c0ff401c9e2f4ac9ce25a90d91
Alex Goley <ajgoley@gmail.com>
2018-01-17 19:51:55 -0500

    api: start code and more deps

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 var compression = require('compression')
 var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
 var app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(compression())
 
 // ROUTING
@@ -9,3 +15,8 @@ app.use('/page', pages)
 
 var templates = require('./controllers/templates.controller.js')
 app.use('/template', templates)
+
+// SERVER CONFIG
+var port = process.env.PORT || 8081;
+// START THE SERVER
+app.listen(port, () => { console.log('ash listening at %s', port) });
